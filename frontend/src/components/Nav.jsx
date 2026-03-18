0e67f62435177f9508612709860c4362c10e3e50
Doggo333333 <43102743+Doggo333333@users.noreply.github.com>
2026-03-18 16:32:50 -0700

    fix: update nav logo icon color to blue (#6EC1FF)

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { ProfileDropdown } from './ProfileDropdown.jsx'
 
 const LogoIcon = () => (
   <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="22" height="22" rx="6" fill="#D97757"/>
+    <rect width="22" height="22" rx="6" fill="#6EC1FF"/>
     <path d="M6 7h10M6 11h10M6 15h6" stroke="#fff" strokeWidth="1.8" strokeLinecap="round"/>
   </svg>
 )
